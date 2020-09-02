2e1554568c839dcd0e8d704d626178d1adec3e95
Ivan <ivanvalverde53@hotmail.com>
2020-09-01 21:20:01 -0300

    Alterações queries

--- QueriesSQL/QueryDez_Top5_ponta.sql
+++ QueriesSQL/QueryDez_Top5_ponta.sql
@@ -1,7 +1,7 @@
 
 /* 5 jogadores que mais sobressairam na ponta */
 
-select max(pa.acceleration+pa.sprint_speed+pa.crossing) as 'ponta', p.player_name 
+select max(pa.acceleration+pa.sprint_speed+pa.crossing+pa.finishing+pa.shot_power) as 'ponta', p.player_name 
 from player_attributes as pa
 inner join player as p on pa.player_api_id = p.player_id
 group by p.player_name
